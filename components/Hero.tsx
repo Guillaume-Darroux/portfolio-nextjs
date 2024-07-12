e21383c3b631c503434b99c180e9c4129614b5e7
guillaume <g.darroux@gmail.com>
2024-07-12 15:25:39 +0200

    Nav section and Grid section in progress

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,7 @@
+import { FaLocationArrow } from "react-icons/fa";
+import MagicButton from "./ui/MagicButton";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
@@ -22,8 +25,22 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic With Next.js
+            Développeur web
           </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Bienvenue sur le portfolio de Guillaume Darroux"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Développeur web freelance.
+          </p>
+          <a href="#about">
+            <MagicButton
+              title="Mes réalisations"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
