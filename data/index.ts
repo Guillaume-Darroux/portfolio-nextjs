5573b68101d644ec4860416efce266816fba5673
guillaume <g.darroux@gmail.com>
2024-07-17 13:40:48 +0200

    Content update

--- data/index.ts
+++ data/index.ts
@@ -38,7 +38,7 @@ export const gridItems = [
   },
   {
     id: 4,
-    title: "Passioné des nouvelles technologies et par le développement.",
+    title: "Passionné des nouvelles technologies et par le développement.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
