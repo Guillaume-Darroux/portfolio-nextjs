158305dc0bb09bd98ca94fd4483d6884206ffafe
guillaume <g.darroux@gmail.com>
2024-07-20 10:51:24 +0200

    Description changed

--- data/index.ts
+++ data/index.ts
@@ -50,7 +50,7 @@ export const gridItems = [
   {
     id: 5,
     title: "En cours de développement de chatbots",
-    description: "The Inside Scoop",
+    description: "Améliorer l'expérience utilisateur",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
