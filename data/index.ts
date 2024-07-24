79a8474201a3e16f6c93c58b4ba9677692b8eae9
guillaume <g.darroux@gmail.com>
2024-07-24 15:44:44 +0200

    Minor update

--- data/index.ts
+++ data/index.ts
@@ -50,7 +50,7 @@ export const gridItems = [
   {
     id: 5,
     title: "En cours de développement de chatbots",
-    description: "Améliorer l'expérience utilisateur",
+    description: "Améliorer l'expérience utilisateur via des chatbots.",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
